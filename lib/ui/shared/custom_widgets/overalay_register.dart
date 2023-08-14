819e4d760890dd9678cb7a2c8b6e6acca41d6e3f
Lieth Apo GaFaR <liethmhmad231@gmail.com>
2023-08-14 14:22:30 +0300

    add overalay_register

--- lib/ui/shared/custom_widgets/overalay_register.dart
+++ lib/ui/shared/custom_widgets/overalay_register.dart
@@ -1,1 +1,24 @@
-st
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:template/ui/shared/colors.dart';
+import 'package:template/ui/shared/custom_widgets/custom_button.dart';
+import 'package:template/ui/shared/custom_widgets/custom_text.dart';
+
+class CustomOveralayRegister extends StatelessWidget {
+  const CustomOveralayRegister({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+
+          SvgPicture.asset('assets/images/ic_key'),
+          CustomText(text: 'يرجى الاشتراك لإتمام عملية التصفح'),
+          CustomButton(text: 'تسجيل الدخول', textColor: AppColors.mainPurple1),
+        ],
+      ),
+    );
+  }
+}
