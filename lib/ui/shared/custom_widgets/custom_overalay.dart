dc843b61ad6277e4d80966738c13846010e16efa
laith <liethmhmad231@gmail.com>
2023-08-15 12:12:22 +0300

    update

--- lib/ui/shared/custom_widgets/custom_overalay.dart
+++ lib/ui/shared/custom_widgets/custom_overalay.dart
@@ -12,49 +12,51 @@ import 'package:template/ui/views/login_view/login_view.dart';
 import 'package:template/ui/views/register_view/register_view.dart';
 
 void showCustomAlertDialog({
-BuildContext? context,
-bool isNotReport = true,
-TextEditingController? report,
+  BuildContext? context,
+  bool isNotReport = true,
+  TextEditingController? report,
 }) {
-Get.dialog(
-AlertDialog(
-  shape: RoundedRectangleBorder(
-    borderRadius: BorderRadius.circular(8.0),
-    side: BorderSide(color: AppColors.mainColor, width: 2.0),
-  ),
-content: Column(mainAxisSize: MainAxisSize.min,
-children: [
-if (isNotReport) ...[
-SvgPicture.asset('assets/images/pop-up.svg'),
-(30).sbh,
-const CustomText(text: 'يرجى الاشتراك لإتمام عملية التصفح'),
-(10).sbh,
-CustomButton(
-text: 'تسجيل الدخول', textColor: AppColors.mainWhite,onPressed: (){
-Get.to(const LoginView());
-},),
-(15).sbh,
-CustomRichText(
-text1: 'ليس لديك حساب',
-text2: 'أنشأ حسابك الان',
-ontap: () {
-Get.to(const RegisterView());
-},
-),
-],
-if (!isNotReport) ...[
-CustomTextField(
-controller: report!,
-// mheight: width *0.5,
-hinttext: 'ارسل شكوى/اقتراح',
-maxLines: 8,
-),
-(10).sbh,
-CustomButton(
-text: 'أرسل', textColor: AppColors.mainWhite),
-]
-],
-),
-),
-);
+  Get.dialog(
+    AlertDialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8.0),
+        side: BorderSide(color: AppColors.mainColor, width: 2.0),
+      ),
+      content: Column(mainAxisSize: MainAxisSize.min,
+        children: [
+          if (isNotReport) ...[
+            SvgPicture.asset('assets/images/pop-up.svg'),
+            (30).sbh,
+            const CustomText(text: 'يرجى الاشتراك لإتمام عملية التصفح'),
+            (10).sbh,
+            CustomButton(
+              text: 'تسجيل الدخول',
+              textColor: AppColors.mainWhite,
+              onPressed: () {
+                Get.to(const LoginView());
+              },),
+            (15).sbh,
+            CustomRichText(
+              text1: 'ليس لديك حساب',
+              text2: 'أنشأ حسابك الان',
+              ontap: () {
+                Get.to(const RegisterView());
+              },
+            ),
+          ],
+          if (!isNotReport) ...[
+            CustomTextField(
+              controller: report!,
+            // mheight: width *0.5,
+              hinttext: 'ارسل شكوى/اقتراح',
+              maxLines: 8,
+            ),
+            (10).sbh,
+            CustomButton(
+                text: 'أرسل', textColor: AppColors.mainWhite),
+          ]
+        ],
+      ),
+    ),
+  );
 }
