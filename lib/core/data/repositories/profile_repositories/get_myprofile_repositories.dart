2159c5042896db83240d414675f090b863352de7
laith-alskaf <laithmhmad231@gmail.com>
2023-08-22 00:10:05 +0300

    Edit API

--- lib/core/data/repositories/profile_repositories/get_myprofile_repositories.dart
+++ lib/core/data/repositories/profile_repositories/get_myprofile_repositories.dart
@@ -1,30 +1,21 @@
-import 'dart:html';
-
 import 'package:dartz/dartz.dart';
 import 'package:template/core/data/models/common_response.dart';
-import 'package:template/core/data/models/profile_model/update_profile_model.dart';
+import 'package:template/core/data/models/profile_model/get_myprofle_model.dart';
 import 'package:template/core/data/network/endpoints/profile_endpoints.dart';
 import 'package:template/core/data/network/network_config.dart';
 import 'package:template/core/enums/request_type.dart';
-import 'package:template/core/enums/request_type_multipart.dart';
 import '../../../utilis/network_utilis.dart';
 
-class UpdatePhotoRepositories {
-  static Future<Either<String, bool>> updatePhoto(
-      {required File photo,}) async {
+class GetMyProfileRepositories {
+  static Future<Either<String, bool>> getMyProfile() async {
     try {
-      return NetworkUtil.postMultipart(
-          method: MultiRequestType.POST,
-          url: ProfileEndPoint.updatePhoto,
-          files: {
-            'photo2':photo.toString(),
-          },
+      return NetworkUtil.sendRequest(type: RequestType.GET,
+          url: ProfileEndPoint.getMyProfile,
           headers: NetworkConfig.getHeaders(
-              extraHeaders: {"Content-Type": "multipart/form-data;"},
               needAuth: true,
-              type: RequestType.POST))
+              type: RequestType.GET))
           .then((respons) {
-        CommonResponse<UpdateProfileModel> commonResponse =
+        CommonResponse<GetMyProfileModel> commonResponse =
         CommonResponse.fromJson(respons);
         if (commonResponse.getstatus && commonResponse.data!.status == true) {
           return Right(commonResponse.getstatus);
--- lib/core/data/repositories/profile_repositories/update_profile_repositories.dart
+++ lib/core/data/repositories/profile_repositories/update_profile_repositories.dart
@@ -1,6 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:template/core/data/models/common_response.dart';
-import 'package:template/core/data/models/update_profile_model.dart';
+import 'package:template/core/data/models/profile_model/update_profile_model.dart';
 import 'package:template/core/data/network/endpoints/profile_endpoints.dart';
 import 'package:template/core/data/network/network_config.dart';
 import 'package:template/core/enums/request_type.dart';
